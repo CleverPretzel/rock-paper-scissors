d1b27cf6ac40416243c97d821121672a61fc7630
Giedre <giedre.klovaite@gmail.com>
2026-02-12 16:42:47 -0600

    Create function to get computer choice

---
+++ script.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+  let randomNum = Math.floor(Math.random() * 3) + 1;
+
+  switch(randomNum) {
+    case 1:
+      return 'rock';
+    case 2:
+      return 'paper';
+    case 3:
+      return 'scissors';
+  }
+
+}
+
+console.log(getComputerChoice());
