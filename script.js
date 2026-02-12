8d6dab48b6efcee96e70c1ab770541bcb0509953
Giedre <giedre.klovaite@gmail.com>
2026-02-12 17:11:20 -0600

    Create function to play a game

--- script.js
+++ script.js
@@ -13,8 +13,39 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  let humanChoice = prompt('rock, paper or scissors?');
-  console.log(humanChoice);
+  return prompt('rock, paper or scissors?').toLowerCase();
 }
 
-getHumanChoice();
+function playGame() {
+
+  let computerScore = 0;
+  let humanScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    if (
+      (humanChoice === 'rock' && computerChoice === 'paper') ||
+      (humanChoice === 'paper' && computerChoice === 'scissors') ||
+      (humanChoice === 'scissors' && computerChoice === 'rock')
+    ) {
+      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      computerScore++;
+    } else if (
+      (humanChoice === 'paper' && computerChoice === 'rock') ||
+      (humanChoice === 'scissors' && computerChoice === 'paper') ||
+      (humanChoice === 'rock' && computerChoice === 'scissors')
+    ) {
+      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      humanScore++;
+    } else {
+      console.log(`Draw! Both chose ${humanChoice}`);
+    }
+  }
+
+  for (i = 0; i < 5; i++) {
+    playRound(getHumanChoice(), getComputerChoice());
+    console.log(`Computer: ${computerScore} Human: ${humanScore}`);
+  }
+
+}
+
+playGame();
